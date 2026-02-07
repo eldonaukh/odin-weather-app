0871c7068c04404303a9ca1de673fbe22fd6ee70
eldonaukh <eldonakh@gmail.com>
2026-02-07 21:59:11 +0800

    Fix index.html path

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './src/index.html',
     }),
+    new Dotenv()
   ],
   module: {
     rules: [
